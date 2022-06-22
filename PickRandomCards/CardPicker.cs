a368507352009a255172bf058d5212525e1f03bb
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-06-22 21:42:43 +0100

    Add project files.

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PickRandomCards
+{
+    class CardPicker
+    {
+        static Random random = new Random();
+
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomValue()
+        {
+            // Random number from 1 - 13
+            int value = random.Next(1, 14);
+            // if 1 then return string Ace
+            if (value == 1) return "Ace";
+            // if 11 then return string Jack
+            if (value == 11) return "Jack";
+            // if 12 then return string Queen
+            if (value == 12) return "Queen";
+            // if 13 then return string King
+            if (value == 13) return "King";
+            // return all other values as string type
+            return value.ToString();
+        }
+
+        private static string RandomSuit()
+        {
+            // Get random number from 1 - 4
+            int value = random.Next(1, 5);
+            // if it's 1 then return string Spades
+            if (value == 1) return "Spades";
+            // if it's 2 then return string Hearts
+            if (value == 2) return "Hearts";
+            // if it's 3 then return string Clubs
+            if (value == 3) return "Clubs";
+            // if nothing returned yet, return string Diamonds
+            return "Diamonds";
+        }
+    }
+}
---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type in the number of cards to pick: ");
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                // executed if line COULD be converted to int that is stored in var numberOfCards
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+
+            }
+            else
+            {
+                // executed if line could NOT be converted to int
+                Console.WriteLine("Invalid input. Please try again.");
+            }
+        }
+    }
+}
